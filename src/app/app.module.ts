5195a19acca58bf794febc6283155bf358524b92
George Flevaris <george.flevaris@thinkrit.com>
2023-07-14 22:36:44 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,42 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {CourseComponent} from './course/course.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {HttpClientModule} from '@angular/common/http';
-import {CourseDialogComponent} from './course-dialog/course-dialog.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {LoginComponent} from './login/login.component';
-import {LessonComponent} from './lesson/lesson.component';
-import {SafeUrlPipe} from './common/safe-url.pipe';
-import {MessagesComponent} from './messages/messages.component';
-import {SearchLessonsComponent} from './search-lessons/search-lessons.component';
-import { LoadingComponent } from './loading/loading.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { CourseComponent } from "./course/course.component";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HttpClientModule } from "@angular/common/http";
+import { CourseDialogComponent } from "./course-dialog/course-dialog.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { LoginComponent } from "./login/login.component";
+import { LessonComponent } from "./lesson/lesson.component";
+import { SafeUrlPipe } from "./common/safe-url.pipe";
+import { MessagesComponent } from "./messages/messages.component";
+import { SearchLessonsComponent } from "./search-lessons/search-lessons.component";
+import { LoadingComponent } from "./loading/loading.component";
+import { CoursesCardListComponent } from "./courses-card-list/courses-card-list.component";
+import { FormsModule } from "@angular/forms";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -46,10 +50,11 @@ import { LoadingComponent } from './loading/loading.component';
     SafeUrlPipe,
     MessagesComponent,
     SearchLessonsComponent,
-    LoadingComponent
-
+    LoadingComponent,
+    CoursesCardListComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
@@ -71,11 +76,11 @@ import { LoadingComponent } from './loading/loading.component';
     MatSelectModule,
     MatDatepickerModule,
     MatMomentDateModule,
-    ReactiveFormsModule
+    MatNativeDateModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
